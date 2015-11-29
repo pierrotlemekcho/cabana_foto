f3c54638fa1d187e450bd90947b1c75e64b767e2
pierrotlemekcho <pierrotlemekcho@gmail.com>
2015-11-29 15:43:52 +0100

    rajout horodatage dictionaire

--- home/pi/teleinfo.py
+++ home/pi/teleinfo.py
@@ -1,15 +1,36 @@
 #!/usr/bin/env python
-import serial, sys
-s = serial.Serial(port='/dev/ttyAMA0', baudrate=1200, bytesize=serial.SEVENBITS, parity=serial.PARITY_EVEN, stopbits=serial.STOPBITS_ONE)
-j=1
-while True :
-    data = s.read(1)
-    j = j+1
-#    print(j)
-#    sys.stdout.write('\ndonnes')
-    if data =='\2':
-         sys.stdout.write('\n===============debut=========')
-    elif data =='\3' :
-         sys.stdout.write('\n==============fin============')
-    else:
-         sys.stdout.write(data)
+import serial
+import datetime
+
+def faire_trame(f):
+    print f 
+
+  
+if __name__ == '__main__' :
+    s = serial.Serial(port='/dev/ttyAMA0', baudrate=1200, bytesize=serial.SEVENBITS, parity=serial.PARITY_EVEN, stopbits=serial.STOPBITS_ONE)
+    carac = None
+    ligne = ''
+    trame = {}
+    while carac != '\x03' : 
+        carac = s.read(1) # attendre une fin de trame
+        while True :
+            carac = s.read(1)
+            if carac == '\x03' : #fin de de trame ,---> trame en traitement
+	        date = str(datetime.datetime.now())
+	        trame['date'] = date
+                faire_trame(trame)
+            elif carac == '\x02': # debut trame on reinitialise la trame
+                trame = {}
+            elif carac == '\n' :#debut ligne on reinitialise la ligne
+                ligne = ''
+            elif carac == '\r' :#fin de ligne traitement donne
+                try :
+                    label, valeur, verif = ligne.split()
+                except ValueError :
+                    continue
+                verif = ord(verif)
+                calcul = (sum(bytearray(label+' '+valeur)) & 0x3f ) + 0x20
+                if verif == calcul : # ligne valide donc prendre en compte
+                    trame[label] = valeur
+            else:
+                ligne = ligne + carac
